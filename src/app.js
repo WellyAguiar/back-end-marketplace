b090f1ca723f9a44baae351913337db234f74263
Seu Nome <seuemail@exemplo.com>
2024-07-08 14:52:35 -0300

    db funcionando com storage

--- src/app.js
+++ src/app.js
@@ -53,7 +53,4 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
